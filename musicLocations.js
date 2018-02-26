3f34368da193835780a2e1b2f194ac8f71d7eb40
Rebekah Watkins <rawa243@uky.edu>
2018-02-26 11:52:32 -0800

    Place objects in separate files

---
+++ musicLocations.js
@@ -0,0 +1,29 @@
+var myMusicLocations = [
+  {
+    name: "Van Andel Arena",
+    properties: {
+      coordinates: [42.962428, -85.671581],
+      type: "Stadium",
+      imageURL: "https://upload.wikimedia.org/wikipedia/commons/c/c0/Vanandel.jpg",
+      icon: "svgs/stadium-11.svg",
+    }
+  },
+  {
+    name: "DeVos Performance Hall",
+    properties: {
+      coordinates: [42.968144, -85.672868],
+      type: "Concert Hall",
+      imageURL: "https://devosperformancehall.com/sites/default/files/styles/performance-basic-page-primary/public/primary-image/DPH_Interior1_0.jpg?itok=TsrkPSyw",
+      icon: "svgs/music-11.svg",
+    }
+  },
+  {
+    name: "Grand Rapids Civic Theatre",
+    properties: {
+      coordinates: [42.964289, -85.667804],
+      type: "Theatre",
+      imageURL: "http://womenslifestyle.com/wp-content/uploads/2013/07/GrandRapidsCivicTheatreExterior1.jpg",
+      icon: "svgs/theatre-11.svg",
+    }
+  }
+];
