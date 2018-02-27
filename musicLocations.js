6469a8f880c6824126aefae5f6f5a3b83965287a
Rebekah Watkins <rawa243@uky.edu>
2018-02-26 18:18:53 -0800

    Update code for concil.log output

--- musicLocations.js
+++ musicLocations.js
@@ -1,29 +1,51 @@
-var myMusicLocations = [
-  {
-    name: "Van Andel Arena",
-    properties: {
-      coordinates: [42.962428, -85.671581],
-      type: "Stadium",
-      imageURL: "https://upload.wikimedia.org/wikipedia/commons/c/c0/Vanandel.jpg",
-      icon: "svgs/stadium-11.svg",
-    }
+var myMusicLocations = {
+  crs: "WGS 84 (EPSG: 4326)", // like it better be!
+  author: "Rebekah Watkins", // metadata keys
+
+  // function to show all names in data
+  describeMusicLocations: function() {
+
+    // declare variables at top of function
+    var myList = "";
+
+    // loop through all features to build list of names
+    for (i = 0; i < this.features.length; i++) {
+        myList += this.features[i].name + ", ";
+
+        // at the end of the list give a final message.
+        if (i === this.features.length-1) {
+          myList += " are all of the places I have mapped!";
+        }
+      }
+    console.log("Report: " + myList );
   },
-  {
-    name: "DeVos Performance Hall",
-    properties: {
-      coordinates: [42.968144, -85.672868],
-      type: "Concert Hall",
-      imageURL: "https://devosperformancehall.com/sites/default/files/styles/performance-basic-page-primary/public/primary-image/DPH_Interior1_0.jpg?itok=TsrkPSyw",
-      icon: "svgs/music-11.svg",
+    // Locations as array of JS objects
+    features: [{
+      name: "Van Andel Arena",
+      properties: {
+        coordinates: [42.962428, -85.671581],
+        type: "Stadium",
+        imageURL: "https://upload.wikimedia.org/wikipedia/commons/c/c0/Vanandel.jpg",
+        icon: "svgs/stadium-11.svg",
+      }
+    },
+    {
+      name: "DeVos Performance Hall",
+      properties: {
+        coordinates: [42.968144, -85.672868],
+        type: "Concert Hall",
+        imageURL: "https://devosperformancehall.com/sites/default/files/styles/performance-basic-page-primary/public/primary-image/DPH_Interior1_0.jpg?itok=TsrkPSyw",
+        icon: "svgs/music-11.svg",
+      }
+    },
+    {
+      name: "Grand Rapids Civic Theatre",
+      properties: {
+        coordinates: [42.964289, -85.667804],
+        type: "Theatre",
+        imageURL: "http://womenslifestyle.com/wp-content/uploads/2013/07/GrandRapidsCivicTheatreExterior1.jpg",
+        icon: "svgs/theatre-11.svg",
+      }
     }
-  },
-  {
-    name: "Grand Rapids Civic Theatre",
-    properties: {
-      coordinates: [42.964289, -85.667804],
-      type: "Theatre",
-      imageURL: "http://womenslifestyle.com/wp-content/uploads/2013/07/GrandRapidsCivicTheatreExterior1.jpg",
-      icon: "svgs/theatre-11.svg",
-    }
-  }
-];
+  ]
+};
--- permenantLocations.js
+++ permenantLocations.js
@@ -1,119 +1,141 @@
-var myLocations = [
-  {
-    name: "John Ball Zoo",
-    properties: {
-      coordinates: [42.962331, -85.702798],
-      exhibits: 11,
-      museum: false,
-      type: "Zoo",
-      imageURL: "https://upload.wikimedia.org/wikipedia/commons/1/14/John_Ball_Zoo_entrance.jpg",
-      icon: "svgs/zoo-11.svg",
-    }
+var myLocations = {
+  crs: "WGS 84 (EPSG: 4326)", // like it better be!
+  author: "Rebekah Watkins", // metadata keys
+
+  // function to show all names in data
+  describePermenantLocations: function() {
+
+    // declare variables at top of function
+    var myList = "";
+
+    // loop through all features to build list of names
+    for (i = 0; i < this.features.length; i++) {
+        myList += this.features[i].name + ", ";
+
+        // at the end of the list give a final message.
+        if (i === this.features.length-1) {
+          myList += " are all of the permenant locations I have mapped!";
+        }
+      }
+    return console.log("Report: " + myList );
   },
-  {
-    name: "Grand Rapids Public Museum",
-    properties: {
-      coordinates: [42.965559, -85.676788],
-      exhibits: 12,
-      museum: true,
-      type: "Museum",
-      imageURL: "https://upload.wikimedia.org/wikipedia/commons/7/77/Grand_Rapids_Public_Museum.jpg",
-      icon: "svgs/museum-11.svg",
-    }
-  },
-  {
-    name: "Gerald R. Ford Museum",
-    properties: {
-      coordinates: [42.968472, -85.677343],
-      exhibits: 2,
-      museum: true,
-      type: "Museum",
-      imageURL: "https://upload.wikimedia.org/wikipedia/commons/8/8d/Ford_ford_museum.jpg",
-      icon: "svgs/museum-11.svg",
-    }
-  },
-  {
-    name: "Grand Rapids Children's Museum",
-    properties: {
-      coordinates: [42.963600, -85.667180],
-      exhibits: 14,
-      museum: true,
-      type: "Museum",
-      imageURL: "https://c2.staticflickr.com/4/3467/3962452789_f26a324a8e_b.jpg",
-      icon: "svgs/museum-11.svg",
-    }
-  },
-  {
-    name: "Grand Rapids Art Museum",
-    properties: {
-      coordinates: [42.965085, -85.671007],
-      exhibits: 2,
-      museum: true,
-      type: "Museum",
-      imageURL: "https://c2.staticflickr.com/6/5705/22301556443_d03521876d_b.jpg",
-      icon: "svgs/museum-11.svg",
+    // Locations as array of JS objects
+    features: [{
+      name: "John Ball Zoo",
+      properties: {
+        coordinates: [42.962331, -85.702798],
+        exhibits: 11,
+        museum: false,
+        type: "Zoo",
+        imageURL: "https://upload.wikimedia.org/wikipedia/commons/1/14/John_Ball_Zoo_entrance.jpg",
+        icon: "svgs/zoo-11.svg",
       }
     },
     {
-      name: "Grand Valley State University - Grand Rapids",
+      name: "Grand Rapids Public Museum",
       properties: {
-        coordinates: [42.964333, -85.680207],
-        type: "College",
-        imageURL: "https://www.gvsu.edu/cms4/asset/6ADCE32C-E6F8-0CD5-23837DD4EB2BE6B2/header/B1556C39-D13E-4660-62DAD32BE6133CF1.jpg",
-        icon: "svgs/college-11.svg",
+        coordinates: [42.965559, -85.676788],
+        exhibits: 12,
+        museum: true,
+        type: "Museum",
+        imageURL: "https://upload.wikimedia.org/wikipedia/commons/7/77/Grand_Rapids_Public_Museum.jpg",
+        icon: "svgs/museum-11.svg",
       }
     },
     {
-      name: "Grand Rapids Community College",
+      name: "Gerald R. Ford Museum",
       properties: {
-        coordinates: [42.966537, -85.666750],
-        type: "College",
-        imageURL: "http://media.mlive.com/grpress/news_impact/photo/grand-rapids-community-college-ddaa354343b54161.jpg",
-        icon: "svgs/college-11.svg",
+        coordinates: [42.968472, -85.677343],
+        exhibits: 2,
+        museum: true,
+        type: "Museum",
+        imageURL: "https://upload.wikimedia.org/wikipedia/commons/8/8d/Ford_ford_museum.jpg",
+        icon: "svgs/museum-11.svg",
       }
     },
     {
-      name: "Founders Brewing Copany",
+      name: "Grand Rapids Children's Museum",
       properties: {
-        coordinates: [42.958376, -85.674171],
-        established: 1994,
-        craft: false,
-        type: "Brewery",
-        imageURL: "https://res.cloudinary.com/simpleview/image/upload/c_fill,f_auto,h_250,q_75,w_750/v1/clients/grandrapids/Founders%20KBS%20Week_9bfb5dfe-b492-41e7-a8ed-4ba99e5b2106.jpg",
-        icon: "svgs/beer-11.svg",
+        coordinates: [42.963600, -85.667180],
+        exhibits: 14,
+        museum: true,
+        type: "Museum",
+        imageURL: "https://c2.staticflickr.com/4/3467/3962452789_f26a324a8e_b.jpg",
+        icon: "svgs/museum-11.svg",
       }
     },
     {
-      name: "New Holland Brewing",
+      name: "Grand Rapids Art Museum",
       properties: {
-        coordinates: [42.970731, -85.679884],
-        established: 1996,
-        craft: true,
-        type: "Brewery",
-        imageURL: "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/grandrapids/New_Holland_Brewing_The_Knickerbocker_c04ca80f-e526-4351-b423-4acec2b7a659.jpg",
-        icon: "svgs/beer-11.svg",
+        coordinates: [42.965085, -85.671007],
+        exhibits: 2,
+        museum: true,
+        type: "Museum",
+        imageURL: "https://c2.staticflickr.com/6/5705/22301556443_d03521876d_b.jpg",
+        icon: "svgs/museum-11.svg",
+        }
+      },
+      {
+        name: "Grand Valley State University - Grand Rapids",
+        properties: {
+          coordinates: [42.964333, -85.680207],
+          type: "College",
+          imageURL: "https://www.gvsu.edu/cms4/asset/6ADCE32C-E6F8-0CD5-23837DD4EB2BE6B2/header/B1556C39-D13E-4660-62DAD32BE6133CF1.jpg",
+          icon: "svgs/college-11.svg",
+        }
+      },
+      {
+        name: "Grand Rapids Community College",
+        properties: {
+          coordinates: [42.966537, -85.666750],
+          type: "College",
+          imageURL: "http://media.mlive.com/grpress/news_impact/photo/grand-rapids-community-college-ddaa354343b54161.jpg",
+          icon: "svgs/college-11.svg",
+        }
+      },
+      {
+        name: "Founders Brewing Copany",
+        properties: {
+          coordinates: [42.958376, -85.674171],
+          established: 1994,
+          craft: false,
+          type: "Brewery",
+          imageURL: "https://res.cloudinary.com/simpleview/image/upload/c_fill,f_auto,h_250,q_75,w_750/v1/clients/grandrapids/Founders%20KBS%20Week_9bfb5dfe-b492-41e7-a8ed-4ba99e5b2106.jpg",
+          icon: "svgs/beer-11.svg",
+        }
+      },
+      {
+        name: "New Holland Brewing",
+        properties: {
+          coordinates: [42.970731, -85.679884],
+          established: 1996,
+          craft: true,
+          type: "Brewery",
+          imageURL: "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/grandrapids/New_Holland_Brewing_The_Knickerbocker_c04ca80f-e526-4351-b423-4acec2b7a659.jpg",
+          icon: "svgs/beer-11.svg",
+        }
+      },
+      {
+        name: "Grand Rapids Brewing Company",
+        properties: {
+          coordinates: [42.963112, -85.670464],
+          established: 1893,
+          craft: true,
+          type: "Brewery",
+          imageURL: "https://static1.squarespace.com/static/5771898929687fe6385a0696/57c0901cbe6594bb753ed4b1/5937005a2e69cfd832923f2b/1514989213107/web+GRBC+Summer+17+051.jpg?format=1500w",
+          icon: "svgs/beer-11.svg",
+        }
+      },
+      {
+        name: "Brewery Vivant",
+        properties: {
+          coordinates: [42.959773, -85.646276],
+          established: 2010,
+          craft: true,
+          type: "Brewery",
+          imageURL: "https://cdn.thecrazytourist.com/wp-content/uploads/2017/02/Brewery-Vivant.jpg",
+          icon: "svgs/beer-11.svg",
+        }
       }
-    },
-    {
-      name: "Grand Rapids Brewing Company",
-      properties: {
-        coordinates: [42.963112, -85.670464],
-        established: 1893,
-        craft: true,
-        type: "Brewery",
-        imageURL: "https://static1.squarespace.com/static/5771898929687fe6385a0696/57c0901cbe6594bb753ed4b1/5937005a2e69cfd832923f2b/1514989213107/web+GRBC+Summer+17+051.jpg?format=1500w",
-        icon: "svgs/beer-11.svg",
-      }
-    },
-    {
-      name: "Brewery Vivant",
-      properties: {
-        coordinates: [42.959773, -85.646276],
-        established: 2010,
-        craft: true,
-        type: "Brewery",
-        imageURL: "https://cdn.thecrazytourist.com/wp-content/uploads/2017/02/Brewery-Vivant.jpg",
-        icon: "svgs/beer-11.svg",
-      }
-    }
-];
+  ]
+};
